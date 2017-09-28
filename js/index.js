02dececcfc5e44d0d4cdaccd98b0791f626cfff6
Rodrigo Gomes Dias <rodrigogmdias@gmail.com>
2017-09-28 11:40:28 -0300

    Water

--- js/index.js
+++ js/index.js
@@ -10,55 +10,28 @@ AMOUNTY = 10,
 camera, scene, renderer;
 
 init();
+initWater();
 animate();
 
 function init() {
 
-  var container, separation = 100, amountX = 50, amountY = 50,
-  particles, particle;
+  var container;
 
   container = document.createElement('div');
   document.body.appendChild(container);
 
-  camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-  camera.position.z = 100;
+  camera = new THREE.PerspectiveCamera( 55, window.innerWidth / window.innerHeight, 0.5, 3000000 );
+  camera.position.set( 2000, 200, 2000 );
 
   scene = new THREE.Scene();
+  scene.fog = new THREE.FogExp2( 0xaabbbb, 0.0001 );
 
-  renderer = new THREE.CanvasRenderer();
+  scene.add( new THREE.AmbientLight( 0x444444 ) );
+
+  renderer = new THREE.WebGLRenderer();
   renderer.setPixelRatio( window.devicePixelRatio );
   renderer.setSize( window.innerWidth, window.innerHeight );
   container.appendChild( renderer.domElement );
-
-
-  var PI2 = Math.PI * 2;
-  var material = new THREE.SpriteCanvasMaterial( {
-    color: 0xffffff,
-    program: function ( context ) {
-      context.beginPath();
-      context.arc( 0, 0, 0.5, 0, PI2, true );
-      context.fill();
-    }
-  } );
-
-  var geometry = new THREE.Geometry();
-
-  for ( var i = 0; i < 100; i ++ ) {
-
-    particle = new THREE.Sprite( material );
-    particle.position.x = Math.random() * 2 - 1;
-    particle.position.y = Math.random() * 2 - 1;
-    particle.position.z = Math.random() * 2 - 1;
-    particle.position.normalize();
-    particle.position.multiplyScalar( Math.random() * 10 + 450 );
-    particle.scale.x = particle.scale.y = 10;
-    scene.add( particle );
-
-    geometry.vertices.push( particle.position );
-  }
-
-  var line = new THREE.Line( geometry, new THREE.LineBasicMaterial( { color: 0xffffff, opacity: 0.5 } ) );
-  scene.add( line );
 
   document.addEventListener( 'mousemove', onDocumentMouseMove, false );
   document.addEventListener( 'touchstart', onDocumentTouchStart, false );
@@ -106,9 +79,12 @@ function animate() {
 }
 
 function render() {
-  camera.position.x += ( mouseX - camera.position.x ) * .05;
-  camera.position.y += ( - mouseY + 200 - camera.position.y ) * .05;
+  // camera.position.x += ( mouseX - camera.position.x ) * .05;
+  // camera.position.y += ( - mouseY + 200 - camera.position.y ) * .05;
   camera.lookAt( scene.position );
+
+  water.material.uniforms.time.value += 1.0 / 60.0;
+  water.render();
 
   renderer.render( scene, camera );
 }
---
+++ js/water.js
@@ -0,0 +1,79 @@
+var water, sphere;
+var parameters = {
+  width: 2000,
+  height: 2000,
+  widthSegments: 250,
+  heightSegments: 250,
+  depth: 1500,
+  param: 4,
+  filterparam: 1
+};
+var waterNormals;
+
+function initWater() {
+  waterNormals = new THREE.TextureLoader().load( 'textures/waternormals.jpg' );
+  waterNormals.wrapS = waterNormals.wrapT = THREE.RepeatWrapping;
+
+  var light = new THREE.DirectionalLight( 0xffffbb, 1 );
+  light.position.set( - 1, 1, - 1 );
+  scene.add( light );
+
+  water = new THREE.Water( renderer, camera, scene, {
+    textureWidth: 512,
+    textureHeight: 512,
+    waterNormals: waterNormals,
+    alpha:  1.0,
+    sunDirection: light.position.clone().normalize(),
+    sunColor: 0xffffff,
+    waterColor: 0x001e0f,
+    distortionScale: 50.0,
+    fog: scene.fog != undefined
+  } );
+
+  var mirrorMesh = new THREE.Mesh(
+    new THREE.PlaneBufferGeometry( parameters.width * 500, parameters.height * 500 ),
+    water.material
+    );
+
+
+  mirrorMesh.add( water );
+  mirrorMesh.rotation.x = - Math.PI * 0.5;
+  scene.add( mirrorMesh );
+
+
+  var cubeMap = new THREE.CubeTexture( [] );
+  cubeMap.format = THREE.RGBFormat;
+  var loader = new THREE.ImageLoader();
+  loader.load( 'textures/skyboxsun25degtest.png', function ( image ) {
+    var getSide = function ( x, y ) {
+      var size = 1024;
+      var canvas = document.createElement( 'canvas' );
+      canvas.width = size;
+      canvas.height = size;
+      var context = canvas.getContext( '2d' );
+      context.drawImage( image, - x * size, - y * size );
+      return canvas;
+    };
+          cubeMap.images[ 0 ] = getSide( 2, 1 ); // px
+          cubeMap.images[ 1 ] = getSide( 0, 1 ); // nx
+          cubeMap.images[ 2 ] = getSide( 1, 0 ); // py
+          cubeMap.images[ 3 ] = getSide( 1, 2 ); // ny
+          cubeMap.images[ 4 ] = getSide( 1, 1 ); // pz
+          cubeMap.images[ 5 ] = getSide( 3, 1 ); // nz
+          cubeMap.needsUpdate = true;
+        } );
+  var cubeShader = THREE.ShaderLib[ 'cube' ];
+  cubeShader.uniforms[ 'tCube' ].value = cubeMap;
+  var skyBoxMaterial = new THREE.ShaderMaterial( {
+    fragmentShader: cubeShader.fragmentShader,
+    vertexShader: cubeShader.vertexShader,
+    uniforms: cubeShader.uniforms,
+    depthWrite: false,
+    side: THREE.BackSide
+  } );
+  var skyBox = new THREE.Mesh(
+    new THREE.BoxGeometry( 1000000, 1000000, 1000000 ),
+    skyBoxMaterial
+    );
+  scene.add( skyBox );
+}
